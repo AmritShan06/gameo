52887f7a1ecb6cc4cf04a7ffad2fb97ee7551ccf
Amritanshu Shandilya <amritshan06@gmail.com>
2024-12-29 11:45:41 +0530

    Add files via upload

---
+++ src/TicTacToeGame/Copyright.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Copyright=()=>{
+    return (
+        <div className="copy">
+            <span>© Copyright - All Rights Reserved</span>
+
+        </div>
+        
+    );
+};
+export default Copyright;
